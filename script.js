2ebb502f2e5200778bb721f0d0b648742f2dbe76
Neeraja Djearaman <neeru.jai@gmail.com>
2019-10-19 15:12:08 +0100

    added code for copying to clipboard

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 // //generate random password
 function generate(){
 let values ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"#$%&(*+,-./:;<=>?@[\]^_{|}'
- passwordLength = prompt("Enter password length");
+ passwordLength = prompt("Enter a password length between 8 and 120");
 let password ="";
+// create for loop to choose password characters
 for(var i=0; i<=passwordLength;i++){
     password = password + values.charAt(Math.floor(Math.random()* Math.floor(values.length-1)));
 }
+// add password to display area
 document.getElementById("display").value =password;
 }
+// function to copy password to clipboard
+function copyPassword(){
+    document.getElementById("display").select();
+    document.execCommand("Copy");
+    alert("Password copied to clipboard")
+}
+
