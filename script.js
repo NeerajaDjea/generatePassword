2faa6e5e7c27abf1a6d0c277ec69190f0cf071f0
Neeraja Djearaman <neeru.jai@gmail.com>
2020-02-05 15:36:06 +0000

    fixed errors

--- script.js
+++ script.js
@@ -1,18 +1,63 @@
-// //generate random password
-function generate() {
-    let values = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"#$%&(*+,-./:;<=>?@[\]^_{|}'
-    passwordLength = prompt("Enter a password length between 8 and 120");
-    let password = "";
-    // create for loop to choose password characters
-    for (var i = 0; i <= passwordLength; i++) {
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+let newPassword = document.querySelector("#displayPassword");
+let generateButton = document.querySelector("#generate");
+let copyButton = document.querySelector("#copy");
+
+
+const lowerCase = `abcdefghijklmnopqrstuvwxyz`;
+const upperCase = `ABCDEFGHIJKLMNOPQRSTUZWXYZ`;
+const specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+const numbers = ` "0123456789"`;
+
+generateButton.addEventListener("click", generatePassword);
+
+
+function generatePassword() {
+
+    let newPassword = " ";
+    let passwordCharacters = "";
+    let passwordLength = prompt("Enter a password length between 8 and 128!");
+
+
+    if (Number(passwordLength < 8) || (passwordLength > 128) || isNaN(passwordLength)) {
+        alert("Please enter valid password length")
+
+    } else {
+        let lowerCaseInput = confirm(" Would you like lowercase letters?")
+        if (lowerCaseInput) {
+            passwordCharacters += lowerCase;
+        }
+
+        let upperCaseInput = confirm(" Would you like upperCase letters?")
+        if (upperCaseInput) {
+            passwordCharacters += upperCase;
+        }
+
+        let numericalInput = confirm(" Would you like numbers?")
+        if (numericalInput) {
+            passwordCharacters += numbers;
+        }
+        let specialCharacterInput = confirm(" Would you like special characters?")
+        if (specialCharacterInput) {
+            passwordCharacters += specialCharacters;
+        }
+
+        if (passwordCharacters === " ") {
+            alert("Please choose type of characters for the password")
+        }
+        for (var i = 0; i < passwordLength; i++) {
+            newPassword += passwordCharacters.charAt(Math.floor(Math.random() * passwordCharacters.length));
+        };
+
+        document.getElementById("displayPassword").value = newPassword;
     }
-    // add password to display area
-    document.getElementById("display").value = password;
-}
-// function to copy password to clipboard
-function copyPassword() {
-    document.getElementById("display").select();
-    document.execCommand("Copy");
-    alert("Password copied to clipboard")
+
+    // function to copy password to clipboard\
+    copyButton.addEventListener("click", copyPassword);
+
+    function copyPassword() {
+        document.getElementById("displayPassword").select();
+        document.execCommand("Copy");
+        document.getElementById("displayPassword").innerHTML = " "
+        alert("Password copied to clipboard")
+    }
 }
